10eca8469cc96778b85401a666a2a2fb93e5f78a
ceinstein <craigjeinstein@gmail.com>
2015-06-11 11:29:55 -0400

    Organized experiemennts and added more rsa executables

--- experiments/pc_count/runexper.sh
+++ experiments/pc_count/runexper.sh
@@ -1,15 +1,35 @@
+#Sets the filename and the rsaname
 filename=$1
 rsaname=$2
+
+#Removes a previous version of the file since the experiment is being redone
 rm $filename.txt
+
+#Changes the directory to asc for convenience
 cd ../../../asc;
+
+#Reads in the file of pcs
 while read line; 
-do ./asc -a $line ../ubasicexper/experiments/$rsaname &> ../ubasicexper/experiments/pc_count//$line.txt;
+
+#Runs the file in asc and puts the output into a file
+do ./asc -a $line ../ubasicexper/experiments/executables/$rsaname &> ../ubasicexper/experiments/pc_count//$line.txt;
+
+#Puts the PC name into the data file
 printf $line" " >> ../ubasicexper/experiments/pc_count/$filename.txt; 
-tail -1 ../ubasicexper/experiments/pc_count/$line.txt | cut -f 3 | xargs >> ../ubasicexper/experiments/pc_count/$filename.txt; 
+
+#Takes the last line in the PC file and puts the excited bit count into the data file
+tail -1 ../ubasicexper/experiments/pc_count/$line.txt | cut -f 3 | xargs >> ../ubasicexper/experiments/pc_count/$filename.txt;
+
+#Finishes reading the file 
 done < ../ubasicexper/experiments/pc_count/rsa_basic_loop_pcs.txt;
+
+#Changes the directory to the experiment for convenience
 cd ../ubasicexper/experiments/pc_count/;
+
+#Remove extraneous files
 rm 4*;
-./plotter.sh;
+
+#Plots the data using gnuplot
 gnuplot <<- EOF                                                                 
    clear
    reset
