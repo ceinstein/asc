0e37b830a789095d639aa0b5806682b9913ddb42
ceinstein <craigjeinstein@gmail.com>
2015-06-12 12:42:18 -0400

    Adds c capabilities

---
+++ experiments/pc_count/mult.sh
@@ -0,0 +1,7 @@
+./runexper.sh rsa31 rsa31
+./runexper.sh rsa37100281 rsa37100281
+./runexper.sh rsa22801754177 rsa22801754177 
+./runexper.sh rsa23098318457041 rsa23098318457041
+./runexper.sh rsa32193219740445481 rsa32193219740445481
+rm huge*
+rm large*
---
+++ experiments/pc_count/multc.sh
@@ -0,0 +1,6 @@
+./runexperc.sh factor22801754177 factor22801754177;
+./runexperc.sh factor23098318457041 factor23098318457041;
+./runexperc.sh factor31 factor31;
+./runexperc.sh factor32193219740445481 factor32193219740445481;
+./runexperc.sh factor37100281 factor37100281;
+./runexperc.sh factor9223371994482243049 factor9223371994482243049;
